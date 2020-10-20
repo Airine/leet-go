cf81d27c6fda71e74d7769c0aabb841d3a947190
airine <airine0119@gmail.com>
2020-10-21 00:59:08 +0800

    2020.10.21 daily

--- src/leet-daily/143.go
+++ src/leet-daily/143.go
@@ -1,77 +1,76 @@
 package main
 
 import (
-	"fmt"
+    "fmt"
 )
 
 //
 type ListNode struct {
-	Val int
-	Next *ListNode
-}
-
-func merge(head *ListNode, tail *ListNode)  {
-	h, t := head, tail
-	for h != nil && t != nil {
-		hn := h.Next
-		tn := t.Next
-		h.Next = t
-		if hn != nil {
-			t.Next = hn
-		}
-		h = hn
-		t = tn
-	}
-}
-
-func reverse(head *ListNode) *ListNode  {
-	if head == nil || head.Next == nil {
-		return head
-	}
-	first, second := head, head.Next
-	head.Next = nil
-	for second != nil {
-		s := second.Next
-		second.Next = first
-		first = second
-		second = s
-	}
-	return first
+    Val int
+    Next *ListNode
 }
 
 func reorderList(head *ListNode)  {
-	l := 0
-	target := head
-	for target != nil {
-		target = target.Next
-		l ++
-	}
-	if l <= 2 {
-		return
-	}
-	target = head
-	for i := 0; i < l/2 - 1; i++ {
-		target = target.Next
-	}
-	rev := target.Next
-	target.Next = nil
-	tail := reverse(rev)
-	merge(head, tail)
+    if head == nil || head.Next == nil {
+        return
+    }
+    // 快慢指针找到中点
+    slow, fast := head, head.Next
+    for fast != nil && fast.Next != nil{
+        slow = slow.Next
+        fast = fast.Next.Next
+    }
+    // 从中点断开
+    middle := slow.Next
+    slow.Next = nil
+    // reverse（逆转）中点以后的list
+    tail := reverse(middle)
+    // merge（归并）两个list
+    merge(head, tail)
+}
+
+func merge(head *ListNode, tail *ListNode)  {
+    h, t := head, tail
+    for h != nil && t != nil {
+        hn := h.Next
+        tn := t.Next
+        h.Next = t
+        if hn != nil {
+            t.Next = hn
+        }
+        h = hn
+        t = tn
+    }
+}
+
+func reverse(head *ListNode) *ListNode  {
+    if head == nil || head.Next == nil {
+        return head
+    }
+    first, second := head, head.Next
+    head.Next = nil
+    for second != nil {
+        s := second.Next
+        second.Next = first
+        first = second
+        second = s
+    }
+    return first
 }
 
 func printListNode(head *ListNode)  {
-	fmt.Print(head.Val)
-	target := head.Next
-	for target != nil {
-		fmt.Printf("-> %d", target.Val)
-		target = target.Next
-	}
-	fmt.Println()
+    fmt.Print(head.Val)
+    target := head.Next
+    for target != nil {
+        fmt.Printf("-> %d", target.Val)
+        target = target.Next
+    }
+    fmt.Println()
 }
 
 func main() {
-	head := &ListNode{Next: &ListNode{Next: &ListNode{Next: &ListNode{Next: &ListNode{Next: nil, Val: 5}, Val: 4}, Val: 3}, Val: 2}, Val: 1}
-	printListNode(head)
-	reorderList(head)
-	printListNode(head)
+    head := &ListNode{Next: &ListNode{Next: &ListNode{Next: &ListNode{Next: nil, Val: 4}, Val: 3}, Val: 2}, Val: 1}
+    printListNode(head)
+    reorderList(head)
+    printListNode(head)
 }
