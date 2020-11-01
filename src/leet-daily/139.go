14873a54b745bb68189f0a406becdf30bc57d68d
Aaron <Airine0119@gmail.com>
2020-11-01 15:44:59 +0800

    solve 140 with public solution

---
+++ src/leet-daily/139.go
@@ -0,0 +1,24 @@
+package main
+
+func wordBreak(s string, wordDict []string) bool {
+    wordSet := map[string]struct{}{}
+    for _, v := range wordDict {
+        wordSet[v] = struct{}{}
+    }
+    dp := make([]bool, len(s)+1)
+    dp[0] = true
+    for i := range s {
+        for j := i+1; j < len(s)+1; j++ {
+            if dp[i] {
+                if _, ok := wordSet[s[i:j]]; ok {
+                    dp[j] = true
+                }
+            }
+        }
+    }
+    return dp[len(s)]
+}
+
+func main() {
+	
+}
